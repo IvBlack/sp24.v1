89187b4910de2348231ea8e89eaf41b48fc653e4
ivan <ivbl21@yandex.ru>
2024-04-08 23:17:43 +0300

    restFull update created

--- catalogue-service/src/main/java/org/chern/catalogue/controller/ProductRestController.java
+++ catalogue-service/src/main/java/org/chern/catalogue/controller/ProductRestController.java
@@ -1,12 +1,21 @@
 package org.chern.catalogue.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.chern.catalogue.controller.payload.UpdateProductPayload;
 import org.chern.catalogue.entity.Product;
 import org.chern.catalogue.service.ProductService;
 import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Locale;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("catalogue-api/products/{productId:\\d+}")
@@ -26,5 +35,32 @@ public class ProductRestController {
     @GetMapping
     public Product getProduct(@ModelAttribute("product") Product product) {
         return product;
+    }
+
+
+    /*
+        Идентификатор товара,
+        изменяемые данные в теле запроса и
+        результат валидации тела запроса.
+    */
+    @PatchMapping
+    public ResponseEntity<?> updateProduct(@Valid @PathVariable("productId") int productId,
+                                           @RequestBody UpdateProductPayload payload,
+                                           BindingResult bindingResult, Locale locale) {
+        if(bindingResult.hasErrors()) {
+            //>>>>>>>>>>>>>>>>>>
+            ProblemDetail problemDetail = ProblemDetail
+                    .forStatusAndDetail(HttpStatus.BAD_REQUEST,
+                            Objects.requireNonNull(this.messageSource.getMessage("errors.400.title",
+                                    new Object[0], "errors.400.title", locale)));
+            problemDetail.setProperty("errors", bindingResult.getAllErrors()
+                    .stream().map(ObjectError::getDefaultMessage).toList());
+            //>>>>>>>>>>>>>>>>>>>
+            return ResponseEntity.badRequest().body(problemDetail);
+        } else {
+            this.productService.updateProduct(productId, payload.title(), payload.details());
+            return ResponseEntity.noContent()
+                    .build();
+        }
     }
 }
