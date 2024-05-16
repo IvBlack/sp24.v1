16dffc71508eff224fd33eb30ab41e9795050b31
Ivan R. <ivbl21@yandex.ru>
2024-05-16 18:29:07 +0300

    repo filtration

--- catalogue-service/src/main/java/org/chern/catalogue/repository/ProductRepository.java
+++ catalogue-service/src/main/java/org/chern/catalogue/repository/ProductRepository.java
@@ -1,9 +1,13 @@
 package org.chern.catalogue.repository;
 
 import org.chern.catalogue.entity.Product;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+//
 public interface ProductRepository extends CrudRepository<Product, Integer> {
 
-    Iterable<Product> findAllByTitleLikeIgnoreCase(String filter);
-}
+    //repo layer filtration
+    @Query(value = "select * from catalogue.t_product е where c_details ilike :filter", nativeQuery = true)
+        Iterable<Product> findAllByTitleLikeIgnoreCase(String filter);
+        }
